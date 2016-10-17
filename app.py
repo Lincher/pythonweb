b9be3310a377c4980cc9d7d30bd5e4cb36044dec
Lincher <lcjive@gmail.com>
2016-10-17 17:57:47 +0800

    ...

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ def index(request):
     return web.Response(body=b"<h1>Awesome</h1>")
 
 @asyncio.coroutine
-def init(loop):
-    app =web.Application(loop=loop)
+def init(loop1):
+    app =web.Application(loop=loop1)
     app.router.add_route("GET","/",index)
     srv = yield from loop.create_server(app.make_handler(),"127.0.0.1",9000)
     logging.info("server started at http://127.0.0.1:9000...")
     return srv
+
+def a():#s
+    pass
     
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
